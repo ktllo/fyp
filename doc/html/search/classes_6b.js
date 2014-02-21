0a11051b211fead1d0289029f038ace6a328e026
ktllo <ktllo@0c88211b-769b-4684-b4fb-8d51a79b13af>
2014-02-21 05:38:02 +0000

    Remove excluded directory

--- doc/html/search/classes_6b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['keywordlist',['KeywordList',['../classorg_1_1leolo_1_1fyp_1_1util_1_1_keyword_list.html',1,'org::leolo::fyp::util']]]
-];
--- doc/html/search/functions_69.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['identify',['identify',['../enumorg_1_1leolo_1_1fyp_1_1util_1_1_keyword_type.html#ac1acfb372074f50305f2295f9ae20071',1,'org::leolo::fyp::util::KeywordType']]]
-];
