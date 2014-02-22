09b6965d42ecaa8fafa4110b549db9c424c7b5e7
LO Kam Tao Leo <ktllo@users.noreply.github.com>
2014-02-22 08:44:52 +0000

    Create a skeleton code for StopwordRemoveThread. Some of the code are being fill in already.

--- ktllo_fyp/src/org/leolo/fyp/util/StopwordRemoverThread.java
+++ ktllo_fyp/src/org/leolo/fyp/util/StopwordRemoverThread.java
@@ -4,8 +4,8 @@
  */
 package org.leolo.fyp.util;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Arrays;
+import org.leolo.fyp.KeywordList;
 
 /**
  *
@@ -16,6 +16,8 @@ public class StopwordRemoverThread extends Thread implements WordCounterUser{
     StringBuffer workspace;
     PostRanker ranker;
     int wordCount=-1;
+    private int pointer=0;//Next character to be scan
+    private int perviousPointer=0;//First character of current word
     
     public StopwordRemoverThread(PostRanker ranker,String string){
         this.ranker = ranker;
@@ -25,7 +27,18 @@ public class StopwordRemoverThread extends Thread implements WordCounterUser{
     @Override
     public void run(){
         //Do some work to remove the stopword
-        //To be fill in...
+        do{
+            String curWord = this.nextWord();
+            if(KeywordType.identify(curWord) == KeywordType.LATIN){
+                if(Arrays.binarySearch(KeywordList.ENGLISH, curWord, String.CASE_INSENSITIVE_ORDER) >= 0){ 
+                    //Remove the stopword
+                    this.removeWord();
+                }
+            }else{
+                //Handle the non-english stopword, with different algroithm to handle them
+                //TODO: Develop an algroithm to handle this case
+            }
+        }while(!isEnd());
         //Assume that the stopwords are removed
         String result = workspace.toString();
         WordCountThread wct = new WordCountThread(this, result);
@@ -55,4 +68,33 @@ public class StopwordRemoverThread extends Thread implements WordCounterUser{
         this.wordCount = value;
     }
     
+    private String nextWord(){
+        //TODO: Fill in pre-process code
+        do{
+            char currentChar = workspace.charAt(pointer++);
+            //TODO: Fill in actual identifying code.
+        }while( pointer < workspace.length());
+        return null;
+    }
+    
+    private void removeWord(){
+        this.workspace.delete(this.perviousPointer, pointer);
+        pointer=perviousPointer;
+    }
+    
+    private void removeCharacter(int start,int end){
+        if(start >= end) {
+            throw new java.lang.IllegalArgumentException("end index must bigger than the start index.");
+        }
+        this.workspace.delete(start, end);
+        pointer-=(end-start);
+    }
+    
+    //Check is finish scaning the StringBuffer
+    private boolean isEnd(){
+        if( this.pointer >= this.workspace.length() ){
+            return true;
+        }
+        return false;
+    }
 }
