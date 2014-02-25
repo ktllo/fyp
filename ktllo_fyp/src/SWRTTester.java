bd3ee080d2ed955503d338478680f7cd59f9b4ba
ktllo <ktllo@0c88211b-769b-4684-b4fb-8d51a79b13af>
2014-02-25 08:06:20 +0000

    hyphen and single quote will not break the word now.

--- ktllo_fyp/src/SWRTTester.java
+++ ktllo_fyp/src/SWRTTester.java
@@ -9,23 +9,7 @@ import org.leolo.fyp.util.StopwordRemoverThread;
  */
 public class SWRTTester {
     public static void main(String [] args){
-        String testString="We came here for the restaurant week. The main reason for choosing this restaurant was that this was the only restaurant available at the time we made reservation.\n" +
-"We were seated at a table across from its open kitchen, from where I could see what the chef was doing.\n" +
-"The food was average but value for money.\n" +
-"The lunch menu was tailor-made for restaurant week.Starter\n" +
-"Smoked salmon salad (good)\n" +
-"Seafood chowder soup(there was not much seafood in the soup and was weak in flavour)\n" +
-"Main course\n" +
-"Grilled flank steak (average) and\n" +
-"mashed potato with cheese (crispy and delicious)\n" +
-"Desert\n" +
-"New York cheesecake (average)\n" +
-"Chocolate bar ( which consisted of 3 layers of different texture was amazing) and vanilla ice- cream (good)\n" +
-"Beverage\n" +
-"Ginger tea (incredibly good)\n" +
-"Cappuccino (smell good, strong flavour)\n" +
-"The manager was friendly. She gave us some discount coupons after the meal.\n" +
-"This hotel restaurant was quite small but cozy. Service was good. Staff was professional. I will recommend this restaurant.";
+        String testString="This is a test-message. Is it a good idea. You couldn\'t disagree with me.";
         StopwordRemoverThread swrt = new StopwordRemoverThread(null,testString);
         System.out.println(testString);
         swrt.removeStopword();
