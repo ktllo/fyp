372921b96e8ab059a16e355b974e6f28b0ccc5fc
lawman0511 <lawman0511@0c88211b-769b-4684-b4fb-8d51a79b13af>
2014-04-19 06:30:41 +0000

    completed the class RankingResult

--- ktllo_fyp/src/org/leolo/fyp/util/RankingResult.java
+++ ktllo_fyp/src/org/leolo/fyp/util/RankingResult.java
@@ -9,13 +9,47 @@ package org.leolo.fyp.util;
  * @author leolo
  */
 public class RankingResult {
-    public int keyword;
-    public int emotionWord;
-    public int word;
-    public int wordWithoutStopword;
-    public int emotionPoint;
+    private int keyword;
+    private int emotionWord;
+    private int word;
+    private int wordWithoutStopword;
+    private int emotionPoint;
+    private int totalPoint;
     
-    public RankingResult(int keyword,int emotionWord,word,wordWithoutStopword,emptionPoint){
-        
+    public RankingResult(int keyword,int emotionWord , int word,int wordWithoutStopword,int emotionPoint){
+        this.keyword=keyword;
+        this.emotionWord=emotionWord;
+        this.word=word;
+        this.wordWithoutStopword=wordWithoutStopword;
+        this.emotionPoint=emotionPoint;
+        this.totalPoint=((this.keyword*40) + (this.emotionWord*20) + 
+                (this.wordWithoutStopword*3) + (this.word*1) );
     }
+    
+    public int getKeyword(){
+        return this.keyword;
+    }
+    
+    public int getEmotionWord(){
+        return this.emotionWord;
+    }
+    
+    public int getWord(){
+        return this.word;
+    }
+    
+    public int getWordWithoutStopword(){
+        return this.wordWithoutStopword;
+    }
+    
+    public int getEmotionPoint(){
+        return this.emotionPoint;
+    }
+    
+    public int getTotalPoint(){
+        return this.totalPoint;
+    }
+            
+                
+            
 }
