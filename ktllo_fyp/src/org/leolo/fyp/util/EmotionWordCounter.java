7367cddfbfc30f5dbbbecd6c96c08d07033c728c
LO Kam Tao Leo <ktllo@users.noreply.github.com>
2014-04-19 05:50:51 +0000

    Fix the EmotionWordCounter to fit the changes in EmotionWordList

--- ktllo_fyp/src/org/leolo/fyp/util/EmotionWordCounter.java
+++ ktllo_fyp/src/org/leolo/fyp/util/EmotionWordCounter.java
@@ -12,31 +12,29 @@ import java.util.*;
  * @author leolo
  */
 public class EmotionWordCounter extends Thread{
-    private EmotionWord word;
     private String target;
     private PostRanker ranker;
     
     private int count = 0;
     private int point = 0;
     
-    public EmotionWordCounter(PostRanker ranker,String target,EmotionWord word){
+    public EmotionWordCounter(PostRanker ranker,String target){
         this.target = target;
-        this.word = word;
         this.ranker = ranker;
     }
     
     @Override
     public void run(){
-        checkList(word.vb,-2);
-        checkList(word.b,-1);
-        checkList(word.g,1);
-        checkList(word.vg,2);
+        checkList(org.leolo.fyp.tmp.EmotionWordList.vb,-2);
+        checkList(org.leolo.fyp.tmp.EmotionWordList.b,-1);
+        checkList(org.leolo.fyp.tmp.EmotionWordList.g,1);
+        checkList(org.leolo.fyp.tmp.EmotionWordList.vg,2);
         this.ranker.emoReport(point, count);
     }
     
-    public void checkList(Vector<Pair> list, int point){
-        for(int i=0;i<list.size();i++){
-            String word = list.get(i).getKey().trim();
+    public void checkList(String [] list, int point){
+        for(int i=0;i<list.length;i++){
+            String word = list[i].trim();
             for(int j=0;j<target.length()-word.length();j++){
                 String sstr = target.substring(j,j+word.length());
                 if(sstr.equalsIgnoreCase(word)){
