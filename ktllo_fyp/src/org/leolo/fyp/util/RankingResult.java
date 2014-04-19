2c018cd96783626c85e7df9cf87fa329a433d658
ktllo <ktllo@0c88211b-769b-4684-b4fb-8d51a79b13af>
2014-04-19 06:00:36 +0000

    New class Ranking resilt

---
+++ ktllo_fyp/src/org/leolo/fyp/util/RankingResult.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leolo.fyp.util;
+
+/**
+ *
+ * @author leolo
+ */
+public class RankingResult {
+    public int keyword;
+    public int emotionWord;
+    public int word;
+    public int wordWithoutStopword;
+    public int emotionPoint;
+    
+    public RankingResult(int keyword,int emotionWord,word,wordWithoutStopword,emptionPoint){
+        
+    }
+}
