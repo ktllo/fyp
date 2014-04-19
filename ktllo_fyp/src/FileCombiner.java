7814634180c0d810e0dbd6301e10f6f9a4bcc948
LO Kam Tao Leo <ktllo@users.noreply.github.com>
2014-04-19 05:45:16 +0000

    Add few class into the repos

---
+++ ktllo_fyp/src/FileCombiner.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+import java.io.*;
+/**
+ *
+ * @author leolo
+ */
+public class FileCombiner {
+    public static void main(String [] args)throws Exception{
+        List<String> el = new Vector<>();
+        List<String> ll = new Vector<>();
+        BufferedReader br = new BufferedReader(new FileReader("vbad.txt"));
+        while(true){
+            String line = br.readLine();
+            if(line == null) break;
+            line = line.trim();
+            if(line.length() == 0) continue;
+            el.add(line);
+        }
+        br = new BufferedReader(new FileReader("bad.txt"));
+        while(true){
+            String line = br.readLine();
+            if(line == null) break;
+            line = line.trim();
+            if(line.length() == 0) continue;
+            ll.add(line);
+        }
+        br.close();
+        dupelim(el);
+        dupelim(ll);
+        Collections.sort(el, String.CASE_INSENSITIVE_ORDER);
+        Collections.sort(ll,String.CASE_INSENSITIVE_ORDER);
+        for(int i=0;i<el.size();i++){
+            for(int j=0;j<ll.size();j++){
+                if(el.get(i).equalsIgnoreCase(ll.get(j)))
+                    ll.remove(j);
+            }
+        }
+        System.out.println();
+    }
+    
+    public static void dupelim(List<String> list){
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(i).equalsIgnoreCase(list.get(j))){
+                    list.remove(j);
+                }
+            }
+        }
+    }
+}
---
+++ ktllo_fyp/src/org/leolo/fyp/tmp/EmotionWordList.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leolo.fyp.tmp;
+
+/**
+ *
+ * @author leolo
+ */
+public class EmotionWordList extends EmotionWord {
+    
+}
